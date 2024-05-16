23435bf36b0eb80e0a57e9cf1d1db69c48134ce6
JoghurtCode <jann.theis@edu.fhdw.de>
2024-05-16 15:27:30 +0200

    initial commit

---
+++ Spring/src/main/java/de/sep/calocalendar/CalocalendarApplication.java
@@ -0,0 +1,13 @@
+package de.sep.calocalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalocalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalocalendarApplication.class, args);
+	}
+
+}
---
+++ Spring/src/test/java/de/sep/calocalendar/CalocalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sep.calocalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalocalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
