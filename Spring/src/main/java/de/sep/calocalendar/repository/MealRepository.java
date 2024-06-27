e182bdb7e0d56e242699bd9433544580f483a88e
janblauer <132467003+janblauer@users.noreply.github.com>
2024-06-27 11:12:24 +0200

    Delete Spring directory
    Keine Änderungen für Spring - Update

--- Spring/src/main/java/de/sep/calocalendar/repository/MealRepository.java
+++
@@ -1,7 +0,0 @@
-package de.sep.calocalendar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import de.sep.calocalendar.entities.Meal;
-
-public interface MealRepository extends JpaRepository<Meal, Long> {
-}
--- Spring/src/main/java/de/sep/calocalendar/repository/UserProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package de.sep.calocalendar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import de.sep.calocalendar.entities.UserProfile;
-
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-}
