dc3fe948f10b5cac98fde5324fb9a15338467512
YoghurtBaum <karl_martin.wilke@edu.fhdw.de>
2024-06-27 12:22:58 +0200

    added Goal Entity, implement Goal Entity into UserProfile as Child Entity, added GoalMapper with GoalMapperTest, edited UserProfileMapperTest and api.yaml -> edited UserProfileModel, added GaolModel

---
+++ Spring/src/main/java/de/sep/calocalendar/entities/Goal.java
@@ -0,0 +1,22 @@
+package de.sep.calocalendar.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Goal {
+
+    @Id
+    private Long id;
+
+    private int goalWeight;
+    private int goalSpeed;
+}
--- Spring/src/test/java/de/sep/calocalendar/mapper/UserProfileMapperTest.java
+++ Spring/src/test/java/de/sep/calocalendar/mapper/UserProfileMapperTest.java
@@ -1,21 +1,44 @@
 package de.sep.calocalendar.mapper;
 
+import de.sep.calocalendar.entities.Goal;
 import de.sep.calocalendar.entities.UserProfile;
+import de.sep.calocalendar.model.GoalModel;
 import de.sep.calocalendar.model.UserProfileModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class UserProfileMapperTest {
 
-    private final UserProfileMapper mapper = new UserProfileMapperImpl();
+    @InjectMocks
+    private UserProfileMapperImpl mapper;
+
+    @Mock
+    private GoalMapper goalMapper;
 
     private UserProfile entity;
     private UserProfileModel model;
+    private Goal goalEntity;
+    private GoalModel goalModel;
 
     @BeforeEach
-    void Setup(){
+    void Setup() {
+        goalModel = new GoalModel();
+        goalModel.setId(1L);
+        goalModel.setGoalWeight(70000);
+        goalModel.setGoalSpeed(10);
+
+        goalEntity = new Goal();
+        goalEntity.setId(1L);
+        goalEntity.setGoalWeight(70000);
+        goalEntity.setGoalSpeed(10);
 
         model = new UserProfileModel();
         model.setId(1L);
@@ -24,6 +47,7 @@ public class UserProfileMapperTest {
         model.setWeight(75000);
         model.setGender(1);
         model.setLevelOfPhysicalActivity(2);
+        model.setGoal(goalModel);
 
         entity = new UserProfile();
         entity.setId(1L);
@@ -32,10 +56,14 @@ public class UserProfileMapperTest {
         entity.setWeight(75000);
         entity.setGender(1);
         entity.setLevelOfPhysicalActivity(2);
+        entity.setGoal(goalEntity);
     }
 
     @Test
     void toEntity_WhenModelProvided_ThenCorrectEntity() {
+        // Mocking the behavior of goalMapper
+        when(goalMapper.toEntity(goalModel)).thenReturn(goalEntity);
+
         var actual = mapper.toEntity(model);
 
         assertThat(actual).isNotNull();
@@ -45,10 +73,19 @@ public class UserProfileMapperTest {
         assertThat(actual.getWeight()).isEqualTo(entity.getWeight());
         assertThat(actual.getGender()).isEqualTo(entity.getGender());
         assertThat(actual.getLevelOfPhysicalActivity()).isEqualTo(entity.getLevelOfPhysicalActivity());
+
+        // Test the Goal mapping
+        assertThat(actual.getGoal()).isNotNull();
+        assertThat(actual.getGoal().getId()).isEqualTo(goalEntity.getId());
+        assertThat(actual.getGoal().getGoalWeight()).isEqualTo(goalEntity.getGoalWeight());
+        assertThat(actual.getGoal().getGoalSpeed()).isEqualTo(goalEntity.getGoalSpeed());
     }
 
     @Test
     void toModel_WhenEntityProvided_ThenCorrectModel() {
+        // Mocking the behavior of goalMapper
+        when(goalMapper.toModel(goalEntity)).thenReturn(goalModel);
+
         var actual = mapper.toModel(entity);
 
         assertThat(actual).isNotNull();
@@ -58,6 +95,11 @@ public class UserProfileMapperTest {
         assertThat(actual.getWeight()).isEqualTo(model.getWeight());
         assertThat(actual.getGender()).isEqualTo(model.getGender());
         assertThat(actual.getLevelOfPhysicalActivity()).isEqualTo(model.getLevelOfPhysicalActivity());
-    }
 
+        // Test the Goal mapping
+        assertThat(actual.getGoal()).isNotNull();
+        assertThat(actual.getGoal().getId()).isEqualTo(goalModel.getId());
+        assertThat(actual.getGoal().getGoalWeight()).isEqualTo(goalModel.getGoalWeight());
+        assertThat(actual.getGoal().getGoalSpeed()).isEqualTo(goalModel.getGoalSpeed());
+    }
 }
