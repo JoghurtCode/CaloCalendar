71f1f1722f129cf335c56962749bd1fcde05a68f
Jannik <jannik.roth97@gmail.com>
2024-06-30 13:30:06 +0200

    ErrorHandling - UpdateButton

--- frontend/src/app/componets/profile/profile.component.ts
+++ frontend/src/app/componets/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ProfileService} from '../../services/profile.service';
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 
 @Component({
@@ -11,22 +12,32 @@ export class ProfileComponent implements OnInit {
 
   profile: any
 
-  constructor(private profileService: ProfileService) {
+  constructor(private profileService: ProfileService, private snackBar: MatSnackBar) {
+
   }
 
   ngOnInit(): void {
     this.loadProfiles();
   }
+
   updateProfile(): void {
     this.profileService.updateUserProfile(this.profile).subscribe(
       (data: any) => {
         this.profile = data;
+        this.snackBar.open('Profile updated successfully', 'Close', {
+          duration: 1000,
+        });
+
       },
       error => {
         console.error('Error updating profile', error);
+        this.snackBar.open('Error updating profile', 'Close', {
+          duration: 2000,
+        });
       }
     );
   }
+
   loadProfiles(): void {
     this.profileService.getUserProfileById(1).subscribe(
       (data: any[]) => {
