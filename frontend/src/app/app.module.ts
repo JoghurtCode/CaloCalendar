539c07aab7bee936374eb75ac7554ae00b946b7a
Jannik <jannik.roth97@gmail.com>
2024-07-03 22:28:43 +0200

    Feat: add frigde & History update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -39,6 +39,7 @@ import {
 } from "@angular/material/table";
 import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
+import { FridgeComponent } from './fridge/fridge.component';
 
 @NgModule({
   declarations: [
@@ -50,6 +51,7 @@ import {MatSort} from "@angular/material/sort";
     RecipesComponent,
     GoalsComponent,
     InventoryComponent,
+    FridgeComponent,
 
   ],
   imports: [
