af724a5834779361e052b8ce41873eee9e141011
Jannik <jannik.roth97@gmail.com>
2024-07-03 23:20:32 +0200

    change goals & dashboard

--- frontend/src/app/componets/goals/goals.component.ts
+++ frontend/src/app/componets/goals/goals.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../services/profile.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ViewStateService } from '../../services/view-state.service';
 
 @Component({
@@ -6,8 +8,50 @@ import { ViewStateService } from '../../services/view-state.service';
   templateUrl: './goals.component.html',
   styleUrls: ['./goals.component.css']
 })
-export class GoalsComponent {
-  constructor(public viewState: ViewStateService) {}
+export class GoalsComponent implements OnInit {
+
+  goal: any = {
+    name: '',
+    targetWeight: 0
+  };
+  profile: any = {};
+
+  constructor(
+    private profileService: ProfileService,
+    private snackBar: MatSnackBar,
+    public viewState: ViewStateService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadProfile();
+    this.loadGoalFromLocalStorage();
+  }
+
+  updateGoal(): void {
+    localStorage.setItem('goalName', this.goal.name);
+    this.snackBar.open('Ziel erfolgreich aktualisiert', 'Schließen', {
+      duration: 1000,
+    });
+  }
+
+  loadProfile(): void {
+    this.profileService.getUserProfileById(1).subscribe(
+      (data: any) => {
+        this.profile = data;
+        this.goal.targetWeight = data.weight;
+      },
+      error => {
+        console.error('Fehler beim Laden des Profils', error);
+      }
+    );
+  }
+
+  loadGoalFromLocalStorage(): void {
+    const storedGoalName = localStorage.getItem('goalName');
+    if (storedGoalName) {
+      this.goal.name = storedGoalName;
+    }
+  }
 
   openDetailView(componentName: string): void {
     this.viewState.setActiveComponent(componentName);
